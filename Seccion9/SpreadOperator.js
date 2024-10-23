ab8817a217055b25d47acd4eb1fc7e1d0089985c
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-10-23 16:43:09 +0200

    Add: Spread ...

---
+++ Seccion9/SpreadOperator.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizerria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruchetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu:['Pizza', 'Pasta', 'Risotto'],
+    openingHours:{
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri:{
+            open: 11,
+            close: 23,
+        },
+        sat:{
+            open: 0,
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function({starterIndex = 1, mainIndex= 0, time ='20:00', address,}){
+        console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+            will be deliver to ${address} at ${time}`);
+    },
+
+    orderPasta: function(ing1, ing2, ing3){
+        console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    }
+};
+
+/* const ingredients = [
+    prompt("Let's make pasta!Ingredient 1?"),
+    prompt("!Ingredient 2?"),
+    prompt("!Ingredient 3?"),
+];
+console-log(ingredients); */
+
+
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// Spread operator
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+// Unpacking to pass like argument  
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, `Gnocci`]; // this is a new array
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Jonas';
+const letters = [ ...str, ' ', 'S.'];
+console.log(letters);
+
+const newRestaurant = {foundedIn: 1998,...restaurant, founder: 'Karlos Arguiñano'};
+console.log(newRestaurant);
+
+const restaurantCopy = {...restaurant};
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
