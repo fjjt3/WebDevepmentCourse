fe5c60a8a2cd26a27a97fd17bf72180b6063ad01
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-10-29 09:25:57 +0100

    Update: Strings

--- Seccion9/Strings.js
+++ Seccion9/Strings.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 const airplane = 'TAP Air Portugal';
 
 console.log(airplane.toLowerCase());
@@ -74,3 +76,21 @@ const checkMiddleSeat = function(seat){
 
 checkMiddleSeat('11B');
 checkMiddleSeat('23C'); */
+
+// String Methods Practice
+
+const getCode = str => str.slice(0 ,3).toLocaleUpperCase();
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+
+
+
+// console.log(flights.split('+'));
+for (const flight of flights.split('+')){
+    const [type, from ,to , time] = flight.split(';');
+    const output = `${type.startsWith('_Delayed')? '🔴':''}  ${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+    console.log(output);
+  }
