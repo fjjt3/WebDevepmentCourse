a051902d1781887a429a7ae52408c18c2096e9fd
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-04 13:41:35 +0100

    Add: Sorting arrays

--- Seccion11/script.js
+++ Seccion11/script.js
@@ -442,4 +442,37 @@ const overallBalance3 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance3);
 
+// Sorting Arrays
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha']
+console.log(owners.sort());
+console.log(owners);
+// sort mitate the arry take care of that, exaple in array of numbers
+
+// Numbers
+console.log(movements);
+// return < 0, A, B (keep order)
+// return > 0 , B, A ( switch order)
+
+// Ascending
+movements.sort((a, b)=> {
+  if (a > b) return 1;
+  if (a < b) return -1;
+});
+
+console.log(movements);
+movements.sort((a,b) => a - b);
+console.log(movements);
+
+// Descending 
+movements.sort((a, b)=> {
+  if (a > b) return -1;
+  if (a < b) return 1;
+});
+
+console.log(movements);
+
+movements.sort((a,b) => b - a);
+console.log(movements);
+
+
 
