af4ea04c0c22ae88086a133bb88f356b25d0badc
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-08 11:57:48 +0100

    Prototypes

--- Seccion14/script.js
+++ Seccion14/script.js
@@ -5,6 +5,34 @@ const Person = function(firstName, birthYear){
     this.birthYear = birthYear;
 };
 
+const matilda = new Person('Matilda', 2017);
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
+
+console.log(jonas instanceof Person);
+///////////////////////////////////////
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
 
