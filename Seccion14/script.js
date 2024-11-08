80a7b21aa620a89bf1f48acda945b373077b3122
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-08 12:39:05 +0100

    Challenge1

--- Seccion14/script.js
+++ Seccion14/script.js
@@ -36,3 +36,60 @@ console.log(jonas.species, matilda.species);
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
 
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function(make, speed){
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate  = function () {
+    this.speed +=10;
+    console.log(`${this.make} is going to ${this.speed} km/h`);
+  };
+
+Car.prototype.brake  = function () {
+    this.speed -=5;
+    console.log(`${this.make} is going to ${this.speed} km/h`);
+  };
+
+
+
+const BMW = new Car('BMW', 120);
+
+BMW.accelerate();
+BMW.brake();
