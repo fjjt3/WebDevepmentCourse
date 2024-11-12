26c2baf3fbf6042f35996c80b62daa8432dd35c1
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-12 10:19:04 +0100

    Inheritance Between Classes: Object.create

--- Seccion14/script.js
+++ Seccion14/script.js
@@ -413,3 +413,32 @@ class PersonC2 {
   martha.introduce();
   martha.calcAge();
   
+// Inheritance Between "Classes": Object.create
+
+const Person1Proto = {
+    calcAge() {
+      console.log(2037 - this.birthYear);
+    },
+  
+    init(firstName, birthYear) {
+      this.firstName = firstName;
+      this.birthYear = birthYear;
+    },
+  };
+  
+  const steven1 = Object.create(Person1Proto);
+  
+  const StudentProto = Object.create(Person1Proto);
+  StudentProto.init = function (firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+  };
+  
+  StudentProto.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  };
+  
+  const jay = Object.create(StudentProto);
+  jay.init('Jay', 2010, 'Computer Science');
+  jay.introduce();
+  jay.calcAge();
