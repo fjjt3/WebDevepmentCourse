d11d8c5ca8426dd6fc723140435c75b708955fed
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-13 10:34:51 +0100

    Callback

--- Seccion16/script.js
+++ Seccion16/script.js
@@ -10,7 +10,7 @@ const countriesContainer = document.querySelector('.countries');
 // Our First AJAX Call: XMLHttpRequest
 
 
-const getCountryData = function (country) {
+ const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://restcountries.com/v2/name/${country}`);
     request.send();
@@ -41,4 +41,56 @@ const getCountryData = function (country) {
   getCountryData('portugal');
   getCountryData('usa');
   getCountryData('germany');
+ 
+  // Welcome to Callback Hell
+
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/name/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
  
