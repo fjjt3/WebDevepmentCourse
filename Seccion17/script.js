2c58c3b007ce9233a3d204a72e723dad8588f208
Francisco Jimenez (ext.) <francisco.jf.jimenez@partner.bmw.de>
2024-11-21 10:51:38 +0100

    Top-Level Await (ES2022)

--- Seccion17/script.js
+++ Seccion17/script.js
@@ -11,3 +11,28 @@ import * as ShoppingCart from './shoppingCart.js'
 ShoppingCart.addToCart('bread', 5);
 
 console.log(ShoppingCart.totalPrice);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+  
+    return { title: data.at(-1).title, text: data.at(-1).body };
+  };
+  
+  const lastPost = getLastPost();
+  console.log(lastPost);
+  
+  // Not very clean
+  // lastPost.then(last => console.log(last));
+  
+  const lastPost2 = await getLastPost();
+  console.log(lastPost2);
